996c32b3594acfe8426d7edb68516bdf3eb294ce
code <mmahad280@gmail.com>
2020-09-05 01:20:50 -0400

    added the navbar componenet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Nav from "./Nav";
 
 function App() {
 	return (
 		<div className="app">
 			<Header />
+			<Nav />
 		</div>
 	);
 }
