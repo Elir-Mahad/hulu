be26e80b2f50f9c8220f7b5f65a69c465ebab319
code <mmahad280@gmail.com>
2020-09-05 02:59:28 -0400

    added the video card component. All movie data should be wired in through this component

--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Results.css";
+import VideoCard from "./VideoCard";
 
 function Results() {
 	return (
 		<div className="results">
 			<h1>results</h1>
+			<VideoCard />
 		</div>
 	);
 }
---
+++ src/VideoCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./VideoCard.css";
+
+function VideoCard({ movie }) {
+	return (
+		<div className="videoCard">
+			<img src="https://cdnb.artstation.com/p/assets/images/images/029/967/403/large/val-orlov-00-render-16-2.jpg?1599171585" />
+			<p>Movie overview</p>
+			<h2>Movie title</h2>
+			<p>number of likes</p>
+		</div>
+	);
+}
+
+export default VideoCard;
