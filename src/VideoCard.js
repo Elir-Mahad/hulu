6ddaa8a920e9211cd7c481e8e05cbb14b401b1cb
code <mmahad280@gmail.com>
2020-09-05 11:34:46 -0400

    added TMBD requests

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -4,7 +4,10 @@ import "./VideoCard.css";
 function VideoCard({ movie }) {
 	return (
 		<div className="videoCard">
-			<img src="https://cdnb.artstation.com/p/assets/images/images/029/967/403/large/val-orlov-00-render-16-2.jpg?1599171585" />
+			<img
+				alt=""
+				src="https://cdnb.artstation.com/p/assets/images/images/029/967/403/large/val-orlov-00-render-16-2.jpg?1599171585"
+			/>
 			<p>Movie overview</p>
 			<h2>Movie title</h2>
 			<p>number of likes</p>
