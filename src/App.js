a5e1317d100fabd8ffb95ebbfa52a6a58819cbbd
code <mmahad280@gmail.com>
2020-09-05 13:51:38 -0400

    here i've changed the way the data gets routed through the app. Previously the in results.js the const request stored a tmbd direct url, but now we want the users selectedOption to dictate what type of url is used for the axios. To do this we used selectedOption as a constant that will store the url thats associated with the users choice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Nav from "./Nav";
 import Results from "./Results";
+import requests from "./request.js";
 
 function App() {
+	//! useState below
+	const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
+	//the trending movies will be always display when the site is first visited
 	return (
 		<div className="app">
 			<Header />
 			<Nav />
-			<Results />
+			<Results selectedOption={selectedOption} />
 		</div>
 	);
 }
--- src/Results.js
+++ src/Results.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./Results.css";
 import VideoCard from "./VideoCard";
 import axios from "./axios.js";
-import requests from "./request.js";
 
-function Results() {
+function Results({ selectedOption }) {
 	//! UseState below
 	const [movies, setMovies] = useState([]);
 	// (movies) The constant movies contains an empty array
@@ -16,12 +15,14 @@ function Results() {
 	useEffect(() => {
 		async function fetchData() {
 			//
-			const request = await axios.get(requests.fetchActionMovies);
+			const request = await axios.get(selectedOption);
 			// (const request = await axios.get)the constant request = will wait for (is expecting) and it will store  'axios.get' (the 'axios.get' itself stores the constant instance from axios.js)
-			// (requests.fetchActionMovies) use the axios command 'requests', to enter into request.js, and fetch the url for action movies
+			// (selectedOption) fetch the url thats currently stored in the constant selectedOption
+
 			// The full url for action movies is: https://api.themoviedb.org/3//discover/movie?api_key=${API_KEY}&with_genres=28
 			// but we don't have to write the full url here because it is already stored in request.js
 			// so all we have to do is import it
+
 			setMovies(request.data.results);
 			// The constant movies contained an empty array
 			// We will now manipulate this empty array
