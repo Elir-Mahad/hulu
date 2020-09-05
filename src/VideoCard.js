547cb094d77500a40c017d5b221c6d2737b4cee1
code <mmahad280@gmail.com>
2020-09-05 13:14:43 -0400

    used react-text-truncate to limit the number of lines for the movie overview. React has so many cool tools .

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./VideoCard.css";
 
+import TextTruncate from "react-text-truncate";
+
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function VideoCard({ movie }) {
@@ -16,9 +18,20 @@ function VideoCard({ movie }) {
 				// or use movie.poster_path to get the movie poster
 				alt=""
 			/>
-			<p>Movie: {movie.overview}</p>
-			{/* grab movie overview */}
-			<h2>Title: {movie.original_title}</h2>
+			<TextTruncate
+				// Grab the movie.overview with the react-text-truncate tool
+				text={movie.overview}
+				// the property text stores the movie overview
+				line={2}
+				// if the text is more than two lines
+				element="p"
+				// take the content of the p element
+				truncateText="…"
+				// cut it of after 2 lines, and at the end of put ...
+				// textTruncateChild={<a href="#">Read more</a>}
+				// in the href above you can insert the link to a url, where the reader can read more
+			/>
+			<h2>{movie.original_title}</h2>
 			{/* grab the movie title */}
 		</div>
 	);
