c90e8fb68138c1dd0a46e1442b426a4f473414d9
code <mmahad280@gmail.com>
2020-09-04 23:26:15 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
 	return (
 		<div className="App">
-			<h1> Hulu </h1>
+			<Header />
 		</div>
 	);
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Header.css";
+
+function Header() {
+	return (
+		<div>
+			<h1> Hulu </h1>
+		</div>
+	);
+}
+
+export default Header;
